27f0f9a905bb4459e1696b0edb29470e4b0e95c4
siddu@0212 <indukethireddy2003@gmail.com>
2023-08-18 18:29:20 +0530

    gamification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Game } from './proj';
+import {BrowserRouter,Route,Routes} from'react-router-dom';
 function App() {
  
   return (
--- frontend/src/proj.js
+++ frontend/src/proj.js
@@ -3,7 +3,8 @@ export const Game=()=>
 {
     return(
         <>
-        <h1><b></b></h1>
+
+       
         <button className="button" onClick={Game}>START</button><br/>
 
         </>
