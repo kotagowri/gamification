938fe733295826f2e60a7db4e7f6a77a7635abb8
siddu@0212 <indukethireddy2003@gmail.com>
2023-08-18 18:22:33 +0530

    gamification

---
+++ frontend/src/proj.js
@@ -0,0 +1,11 @@
+import React from "react";
+export const Game=()=>
+{
+    return(
+        <>
+        <h1><b></b></h1>
+        <button className="button" onClick={Game}>START</button><br/>
+
+        </>
+    )
+}
