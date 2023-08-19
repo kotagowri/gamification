acb6bb2173654b4e5af2c96c051e84b004f2d3f5
siddu@0212 <indukethireddy2003@gmail.com>
2023-08-19 22:26:17 +0530

    gamification

--- frontend/src/proj.js
+++ frontend/src/proj.js
@@ -5,20 +5,23 @@ export const Game=()=>
 {
     return(
         <>
-        <div className="main">
-            
+        
+        <div className="main"> 
+           <div className="text">
+           <h1>WELCOME</h1>
+        </div>  
+        click on the image to play game😊 <br/>
             <div className="sd">
-       
        <a href="https://poki.com/en/g/11-11">
-       <img src="block.jpg"width={250}height={250}></img><br/>
+       <img src="block.jpg"width={150}height={150}></img><br/>
         </a><br/></div>
         <div className="sd">
        <a href="https://poki.com/en/g/ludo-multiplayer">
-       <img src="ludo.jpg"width={250}height={250}></img><br/>
+       <img src="ludo.jpg"width={150}height={150}></img><br/>
         </a><br/></div>
         <div className="sd"> 
        <a href="https://poki.com/en/g/words-search-classic-edition">
-       <img src="word.jpg"width={250}height={250}></img><br/></a><br/></div>
+       <img src="word.jpg"width={150}height={150}></img><br/></a><br/></div>
         </div>
         </>
     )
