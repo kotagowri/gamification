88d0f35a3cf7f4c1a23aad984dd705c9e1c5de65
kotagowri <kotagowri2003@gmail.com>
2023-08-25 18:24:10 +0530

    gamification

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,17 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 app.get('/',(req,res)=>{
+
     res.send("server is running")
 })
 app.post('/update/:email/:temp',async (req,res)=>{
   
     const details = await db.collection("game").findOneAndUpdate({Email:req.params.email},{$set:{Gamepoints:req.params.temp}});
     res.json(details);
+})
+app.post('/find1/:Name/:Datee/:streak',async(req,res)=>{
+    const data1=await db.collection('game').findOneAndUpdate({Name:req.params.Name},{$set:{Date:req.params.Datee,Streak:req.params.streak}});
+    res.json(data1);
 })
 app.get('/show/:email',async(req,res)=>
 {
--- frontend/src/proj.js
+++ frontend/src/proj.js
@@ -1,17 +1,61 @@
-import React, { useEffect, useState } from "react";
-import {BrowserRouter,Route,Routes,Link} from 'react-router-dom';
+import React, { useEffect, useState  } from "react";
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
+
+
 
 export const Game=()=>
 {
     let gamepoints=0;
-    const[count,setCount]=useState(0);
-    const Increment=()=>{
-        setCount(count+1);
+    const nav=useNavigate();
+    const[streak,setStreak]=useState(0);
+    const[count,setCount]=useState('');
+    const[email,setEmail]=useState([]);
+    const locale='en';
+    const today=new Date();
+    const day = today.toLocaleDateString(locale, { weekday: 'long' });
+    const year = today.toLocaleDateString(locale, { year: 'numeric' });
+    const date = `${day}, ${today.getDate()} ${today.toLocaleDateString(locale, { month: 'long' })},${year}\n\n`;
+    const Disable=()=>{
+        document.getElementById("ludo").disabled=true;
+
+    }
+    const Increment=async()=>{
+    try{
+        const res1=await axios.get('http://localhost:8000/find1/'+email)
+        if(Date.parse(date)===Date.parse(res1.data.Date))
+        {
+                 setStreak(res1.data.Streak);
+        }
+        else{
+            setStreak(parseInt(res1.data.Streak)+1);
+        }
+        const res=await axios.get('http://localhost:8000/show/'+email)
+        {
+            if(res.data){
+                gamepoints=parseInt(res.data.Gamepoints)+1;
+                const res1=await axios.post('http://localhost:8000/update/'+email+"/"+gamepoints)
+                {
+                    if(res1.data){
+                        setCount(gamepoints);
+                    }
+                    else{
+                        alert("no")
+                    }
+                }
+            }
+            else
+            {
+                nav('/');
+                // alert('cant find')
+            }
+        }
+    }
+    catch(e){
+        console.log(e);
     }
 }
-    // useEffect(()=>{
-    //     console.log("UseEffecting called");
-    // },[count]
+   
     return(
         <>
         
@@ -20,27 +64,31 @@ export const Game=()=>
            <h1>WELCOME</h1>
         </div> 
         <h2 className="h2"> game points: {count} </h2> 
-        <marquee><h3>click on the image to play game😊 </h3></marquee><br/>
-        {/* {count} */}
+        <h3>click on the image to play game😊 </h3><br/>
+        <div>
+            <input type="email" onChange={(e)=>setEmail(e.target.value)}></input>
+            <button onClick={Increment}>enter</button>
+        </div>
+
             <div className="sd">
        <a href="https://poki.com/en/g/11-11" target="_blank">
        <img onClick={Increment} src="block.jpg" alt="block" width={150}height={150}></img></a><br/>
         <br/></div>
 
         <div className="sd">
-       <a href="https://poki.com/en/g/ludo-multiplayer" target="_blank">
+       <a href="https://poki.com/en/g/ludo-multiplayer" target="_blank" id="ludo">
        <img onClick={Increment} src="ludo.jpg" alt="ludo" width={150}height={150}></img><br/>
-        </a><br/></div>
+        </a><br/>{Disable}</div>
 
         <div className="sd"> 
        <a href="https://poki.com/en/g/words-search-classic-edition" target="_blank">
        <img onClick={Increment} src="word.jpg" alt="word" width={150}height={150}></img><br/></a><br/></div>
+       
 
         <div className="sd"> 
        <a href="/games/dicee" >
        <img src="dice.png" alt="dice" width={150}height={150}></img><br/></a><br/></div>
         </div>
-
         </>
     )
 }
